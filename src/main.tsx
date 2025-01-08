b7fee3660eb0b140a562a286bc1c4553749dabd3
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-01-08 12:08:15 +0600

    intregrate redux persist for state persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/index.tsx'
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/index.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router}>
-
-    </RouterProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/baseApi";
 import userReducer from "./features/userSlice"
+import { persistReducer, persistStore } from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
+
+
+const persistUserConfig = {
+  key:'user',
+  storage
+}
+
+const persistedUserReducer = persistReducer(persistUserConfig,userReducer)
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    user:userReducer
+    user:persistedUserReducer
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware({serializableCheck:false}).concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+
+
+export const persistor = persistStore(store)
